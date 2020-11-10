164de353fe8c695c1b2ea6454c5b328ee847aacc
Смирнов Егор <smegr@mail.ru>
2020-11-10 12:44:35 +0300

    HomeWork

---
+++ src/ru/mirea/ikbo1319/Practical_5/Ex1/Ex1.js
@@ -0,0 +1,26 @@
+//Задача 1.1
+let num = 3;
+alert("Num = " + num);
+//Задача 1.2
+let a = 10;
+let b = 2;
+alert("Сумма - " + (a + b) + "\n" + "Разность - " + (a - b) + "\n" +
+    "Произведение - " + (a * b) + "\n" + "Частное - " + (a / b));
+//Задача 1.3
+let c = 15;
+let d = 2;
+let result = c + d;
+alert("Result = " + result);
+//Задача 1.4
+a = 10;
+b = 2;
+c = 5;
+alert("Сумма a+b+c = " + (a + b + c));
+//Задача 1.5
+a = 17;
+b = 10;
+c = a - b;
+d = 7;
+result = c + d;
+alert("Result = " + result);
+
