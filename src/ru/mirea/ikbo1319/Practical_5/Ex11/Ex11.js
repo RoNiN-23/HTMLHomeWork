b480b73e90efee838f30f5705a45a11346d72f48
Смирнов Егор <smegr@mail.ru>
2020-11-13 16:32:44 +0300

    HomeWork

---
+++ src/ru/mirea/ikbo1319/Practical_5/Ex11/Ex11.js
@@ -0,0 +1,13 @@
+//Задача 11.1
+let test = true;
+if(test == true)alert("Верно");
+else alert("Неверно");
+
+if(test)alert("Верно");
+else alert("Неверно");
+//Задача 11.2
+if(test != true)alert("Верно");
+else alert("Неверно");
+
+if(!test)alert("Верно");
+else alert("Неверно");
