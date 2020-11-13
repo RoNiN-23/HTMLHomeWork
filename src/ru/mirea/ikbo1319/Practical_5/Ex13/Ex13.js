42dd76923994840c44377bb8b6fa5503211324b0
Смирнов Егор <smegr@mail.ru>
2020-11-13 17:24:36 +0300

    HomeWork

---
+++ src/ru/mirea/ikbo1319/Practical_5/Ex13/Ex13.js
@@ -0,0 +1,12 @@
+let num = 2;
+let result;
+switch (num) {
+    case 1:alert("Зима");
+        break;
+    case 2:alert("Весна");
+        break;
+    case 3:alert("Лето");
+        break;
+    case 4:alert("Осень");
+        break;
+}
